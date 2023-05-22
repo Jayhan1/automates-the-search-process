ea811ffd6b3505f3bcc15c8a89ee637b53c00079
Jayhan alnofli <130177092+Jayhan1@users.noreply.github.com>
2023-05-22 10:04:47 +0400

    Add files via upload

---
+++ stack.py
@@ -0,0 +1,22 @@
+
+stack = []
+ 
+stack.append('H')
+stack.append('E')
+stack.append('L')
+stack.append('L')
+stack.append('O')
+print(stack)
+ 
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+ 
+
+
+    
+
+
+    
