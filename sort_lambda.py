8959c168a1dfbc843c78f8218ce22d49d8af3ce4
Jayhan alnofli <130177092+Jayhan1@users.noreply.github.com>
2023-05-24 09:55:45 +0400

    Add files via upload

---
+++ sort_lambda.py
@@ -0,0 +1,8 @@
+
+people = [{'name':'John','age':28},{'name':'Mary','age':23},
+          {'name':'Bob','age':35},{'name':'Alice','age':32}]
+sorted_list = sorted(people,key=lambda x:x['age'])
+
+print("Sorted:",sorted_list)
+
+
